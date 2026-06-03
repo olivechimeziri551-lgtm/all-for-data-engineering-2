917bd548915926cab1d1e3fafd67953eafd2df1b
olivechimeziri551-lgtm <olivechimeziri@outlook.com>
2026-06-03 23:22:04 +0100

    update on my code

---
+++ var1.py
@@ -0,0 +1,45 @@
+# var1 = "obi"
+# print (type (var1))
+# var1 = 25.5
+# print (type (var1))
+# var1 = "A"n
+# var2 = "a"
+# print (ord (var1))
+# print (ord (var2))
+# x = str(var1)
+# print (x)
+# print (type (x))
+# x = "y" "orange" "apple"
+# prit (x)
+# x,y,"oz = range", "apple", "banana"
+# print(x)
+# prit(y)
+# print(z)
+var1 = "obi is a boy"
+# for i in var1:
+#     print(i)
+    
+#     var = "obi"
+#     print(len(var))
+# print("boy" in var1)
+# var = "obi is a boy"
+# print(var[0])
+# print(var[2:5])
+# print(var [1:6])
+# print(var[2:])
+# print(var[:3])
+# print(var[1:8:2])
+# var = "informationtechnology"
+# print(var[1:8:2])
+# print(var[1::3]
+# var = ["orange", "apple", "mango","banana","grape","cucumber","papaya","guava","watermelon"]
+# print(var[0])
+# print(var[4])
+# print(var[4][0])
+var = "obi is a boy"
+# print(var1.upper())
+# print(var1.capitalize())
+# var = " obi is a boy "
+# print(var.strip())
+var2 = " ada is a girl"
+print(var1 + var2)
